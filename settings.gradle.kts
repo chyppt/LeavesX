a38fcf0d4e172ee7ec7d0b5a2c630a95131bfd1b
LeavesX Build <leavesx-build@local>
2026-08-28 01:18:19 +0800

    Add LeavesX 1.20.6 build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
@@ -9,6 +9,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
 
-rootProject.name = "Leaves"
+rootProject.name = "LeavesX"
 
 include("leaves-api", "leaves-server", "paper-api-generator")
